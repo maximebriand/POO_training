2f18ff3037f28880fe3b7a220db3faf8a52dfdb6
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2017-03-23 11:33:53 +0100

    chapitre 15

--- blog/app/App.php
+++ blog/app/App.php
@@ -11,31 +11,14 @@ namespace App;
 
 class App
 {
-    const DB_NAME = 'blog';
-    const DB_USER = 'root';
-    const DB_PASS = 'root';
-    const DB_HOST = 'localhost';
+    public $title = "Mon Super Site";
+    private static $_instace;
 
-
-    private static $database;
-    private static $title = "mon super site";
-
-    public static function getDb(){
-        if(self::$database === null){
-            self::$database =  new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
+    public static function getInstance(){
+        if (is_null(self::$_instace)){
+            self::$_instace = new App();
         }
-        return self::$database;
+        return self::$_instace;
     }
 
-    public static function notFound(){
-        header("HTTP/1.0 404 Not Found");
-        header('Location:index.php?p=404');
-    }
-
-    public static function getTitle(){
-        return self::$title;
-    }
-    public static function setTitle($title){
-        self::$title = $title;
-    }
 }
---
+++ blog/app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maximebriand
+ * Date: 23/03/2017
+ * Time: 11:07
+ */
+
+return array(
+    "db_user" => "root",
+    "db_pass" => "root",
+    "db_host" => "localhost",
+    "db_name" => "blog"
+);
